0571310c61cf0b97de01cd02495e23b91922f6b7
MEREE98 <66564063+MEREE98@users.noreply.github.com>
2020-06-20 15:13:11 +0430

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,67 @@
+package Node;
+import Component.*;
+
+import java.util.ArrayList;
+
+public class Node {
+    double voltage;
+    double voltagederive;
+    double voltageintegral;
+    double currentleaving;
+    int number;
+    protected ArrayList<Branch> attachments;
+    double time;
+
+    protected Node(int number){
+        this.number=number;
+    }
+    protected Node(int number, ArrayList<Branch> attachments){
+        this.number=number;
+        this.attachments=attachments;
+    }
+
+    protected Node() {
+    }
+
+
+
+    public int getnumber() {
+        return number;
+    }
+
+    public void setVoltage(double voltage) {
+        this.voltage = voltage;
+    }
+
+
+    public double getVoltage() {
+        return voltage;
+    }
+
+
+    public void setVoltagederive(double voltagederive) {
+        this.voltagederive=voltagederive;
+    }
+
+    public double getVoltagederive() {
+        return voltagederive;
+    }
+
+    public void setCurrentleaving(double currentleaving){
+      this.currentleaving=currentleaving;
+    }
+
+    public double getCurrentleaving() {
+        return currentleaving;
+    }
+
+    public void setVoltageintegral(double voltageintegral) {
+      this.voltageintegral=voltageintegral;
+    }
+
+
+    public double getVoltageintegral() {
+        return voltageintegral;
+    }
+
+}
